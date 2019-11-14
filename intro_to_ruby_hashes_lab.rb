eac2f635df0d9528fc620755f1dc800e118a9858
Patrick Richard <prichard191@gmail.com>
2019-11-14 04:53:59 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,7 +24,7 @@ def monopoly_with_third_tier
   :railroads =>
     {:pieces=> 4,
 		 :rent_in_dollars=>{:one_piece_owned=>25,:two_pieces_owned=>50,:three_pieces_owned=>100,:four_pieces_owned=>200},
-		 :names=>{:shortline_railroad=>{},:b_and_o_railroad=>{},:pennsylvania_railroad=>{},:reading_railroad=>{}}
+		 :names=>{:reading_railroad=>{:"mortgage_value"=>"$100"},:pennsylvania_railroad=>{},:b_and_o_railroad=>{},:shortline_railroad=>{}}
 
 
 		 }
